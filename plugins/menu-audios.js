87a36eedde4a7b7237af2670c4818b4f30f67fce
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-10-05 23:49:25 -0400

    Actualizar menu-audios.js

--- plugins/menu-audios.js
+++ plugins/menu-audios.js
@@ -21,7 +21,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
 let tags = {
 'audios': '𝐀𝐔𝐃𝐈𝐎𝐒',
 }
-let img = 'https://i.ibb.co/YcdFyft/file.jpg'
+let img = 'https://iili.io/dsS9lja.md.png'
 
   try {
           // DEFAULT MENU
