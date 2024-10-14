dd2ed3f5e5cf8de3ed6e801ae2c5e9cf10a45add
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-14 09:34:15 -0300

    Actualizar owner-resetprefix.js

--- plugins/owner-resetprefix.js
+++ plugins/owner-resetprefix.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn}) => {
   global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
   //await m.reply(`✅️ *Prefijo Restablecido Con Éxito!*`);
-  conn.fakeReply(m.chat, '✅️ *Prefijo Restablecido Con Éxito!*', '0@s.whatsapp.net', '🤍 PREFIJO RESTABLECIDO')
+  conn.fakeReply(m.chat, '> ✅️ᮬ᭄ *Prefijo Restablecido Con Éxito!*', '0@s.whatsapp.net', '> 👒 PREFIJO RESTABLECIDO')
 };
 handler.help = ['resetprefix'];
 handler.tags = ['owner'];
