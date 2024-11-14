058a26e78f632b6f7cba02ebcf2a1322931ca8e0
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-14 14:40:16 -0400

    Add files via upload

---
+++ plugins/nsfw-nsfw.js
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (db.data.chats[m.chat].nsfw == false && m.isGroup) return conn.reply(m.chat, `✧ El nsfw no esta activado, pidele a un admin que lo active escribiendo: *.on nsfw*`)
+let res = await fetch(`https://fantox-apis.vercel.app/${command}`)
+await m.react('🕓')
+try {
+if (!res.ok) throw await res.text()
+let json = await res.json()
+if (!json.url) throw m.react('✖️')
+await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', `*✧ Random* : ${command}`, m, null, rcanal)
+await m.react('✅')
+} catch {
+await m.react('✖️')
+}}
+handler.help = ['genshin', 'swimsuit', 'schoolswimsuit', 'white', 'barefoot', 'touhou', 'gamecg', 'hololive', 'uncensored', 'sunglasses', 'glasses', 'weapon', 'shirtlift', 'chain', 'fingering', 'flatchest', 'torncloth', 'bondage', 'demon', 'wet', 'pantypull', 'headdress', 'headphone', 'tie', 'anusview', 'shorts','stokings', 'topless', 'beach', 'bunnygirl', 'bunnyear', 'idol', 'vampire', 'gun', 'maid', 'bra', 'nobra', 'bikini', 'whitehair', 'blonde', 'pinkhair', 'bed', 'ponytail', 'nude', 'dress', 'underwear', 'foxgirl', 'uniform', 'skirt', 'sex', 'sex2', 'sex3', 'breast', 'twintail', 'spreadpussy', 'tears', 'seethrough', 'breasthold', 'drunk', 'fateseries', 'spreadlegs', 'openshirt', 'headband', 'food', 'close', 'tree', 'nipples', 'erectnipples', 'horns', 'greenhair', 'wolfgirl', 'catgirl']
+handler.command = ['genshin', 'swimsuit', 'schoolswimsuit', 'white', 'barefoot', 'touhou', 'gamecg', 'hololive', 'uncensored', 'sunglasses', 'glasses', 'weapon', 'shirtlift', 'chain', 'fingering', 'flatchest', 'torncloth', 'bondage', 'demon', 'wet', 'pantypull', 'headdress', 'headphone', 'tie', 'anusview', 'shorts','stokings', 'topless', 'beach', 'bunnygirl', 'bunnyear', 'idol', 'vampire', 'gun', 'maid', 'bra', 'nobra', 'bikini', 'whitehair', 'blonde', 'pinkhair', 'bed', 'ponytail', 'nude', 'dress', 'underwear', 'foxgirl', 'uniform', 'skirt', 'sex', 'sex2', 'sex3', 'breast', 'twintail', 'spreadpussy', 'tears', 'seethrough', 'breasthold', 'drunk', 'fateseries', 'spreadlegs', 'openshirt', 'headband', 'food', 'close', 'tree', 'nipples', 'erectnipples', 'horns', 'greenhair', 'wolfgirl', 'catgirl']
+handler.tags = ['nsfw'] 
+handler.premium = false 
+handler.register = true 
+//handler.limit = 10
+handler.group = true 
+
+export default handler
---
+++ plugins/search-ttsearch.js
@@ -0,0 +1,102 @@
+import axios from 'axios'
+const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = (await import('@adiwajshing/baileys')).default
+
+let handler = async (m, {
+    conn,
+    args,
+    text,
+    usedPrefix,
+    command
+}) => {
+try {
+await m.reply(wait)
+
+let { title, no_watermark } = await tiktoks(text);
+
+let msg = generateWAMessageFromContent(m.chat, {
+  viewOnceMessage: {
+    message: {
+        "messageContextInfo": {
+          "deviceListMetadata": {},
+          "deviceListMetadataVersion": 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: `\`${title}\``
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: wm
+          }),
+          header: proto.Message.InteractiveMessage.Header.create({
+            hasMediaAttachment: false,
+           ...await prepareWAMessageMedia({ video: { url: no_watermark } }, { upload: conn.waUploadToServer })
+          }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons: [
+            {
+              "name": "quick_reply",
+              "buttonParamsJson": `{"display_text": "Mas resultados", "id": ".ttsearch ${text}"}`
+              }
+              ],
+          })
+        })
+    }
+  }
+}, {quoted: m, userJid: m})
+
+conn.relayMessage(m.chat, msg.message, {
+  messageId: msg.key.id,
+})
+
+} catch (e) {
+throw eror
+}
+}
+handler.help = ['ttiktoksearch']
+handler.tags = ['search']
+handler.command = /^(ttsearch|tiktoksearch)$/i
+handler.limit = true 
+handler.register = true
+
+export default handler
+
+async function tiktoks(query) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await axios({
+        method: 'POST',
+        url: 'https://tikwm.com/api/feed/search',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          'Cookie': 'current_language=en',
+          'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36'
+        },
+        data: {
+          keywords: query,
+          count: 10,
+          cursor: 0,
+          HD: 1
+        }
+      });
+      const videos = response.data.data.videos;
+      if (videos.length === 0) {
+        reject("Tidak ada video ditemukan.");
+      } else {
+        const gywee = Math.floor(Math.random() * videos.length);
+        const videorndm = videos[gywee]; 
+
+        const result = {
+          title: videorndm.title,
+          cover: videorndm.cover,
+          origin_cover: videorndm.origin_cover,
+          no_watermark: videorndm.play,
+          watermark: videorndm.wmplay,
+          music: videorndm.music
+        };
+        resolve(result);
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
