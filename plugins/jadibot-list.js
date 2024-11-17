06b8f55bb4dcdb19a124d2f9dcfd6b5bd8e82b53
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-17 16:04:38 -0400

    Actualizar jadibot-list.js

--- plugins/jadibot-list.js
+++ plugins/jadibot-list.js
@@ -1,4 +1,4 @@
-import { jidNormalizedUser } from "@adiwajshing/baileys";
+/* import { jidNormalizedUser } from "@adiwajshing/baileys";
 import Jadibots from "../lib/jadibots.js";
 let handler = async (m, { usedPrefix }) => {
     const users = [...Jadibots.conns.entries()].map(([k, v]) => v.user);
@@ -15,4 +15,35 @@ handler.help = ['listjadibot'];
 handler.tags = ['jadibot'];
 handler.command = /^(list(jadi)?bot|(jadi)?botlist)$/i;
 
-export default handler;
+export default handler; */
+
+import ws from 'ws'
+
+async function handler(m, { conn: stars, usedPrefix }) {
+  let uniqueUsers = new Map()
+
+  global.conns.forEach((conn) => {
+    if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
+      uniqueUsers.set(conn.user.jid, conn)
+    }
+  })
+
+  let users = [...uniqueUsers.values()]
+
+  let img = await (await fetch(`https://i.ibb.co/LSBGpg4/file.jpg`)).buffer()
+  let message = users.map((v, index) => `
+*[ \`${index + 1}\` -  ${v.user.name || 'Sin Nombre'} ]*\n* *🤍 \` Link :\`* https://wa.me/${v.user.jid.replace(/[^0-9]/g , '')}\n`).join('\n\n')
+
+  let replyMessage = message.length === 0 ? '' : message
+  global.totalUsers = users.length
+  let responseMessage = `*[ _Total Subbots Activos :_ \`${totalUsers || '0'}\` ]*\n\n${replyMessage.trim()}`.trim()
+
+await stars.sendFile(m.chat, img, 'thumbnail.jpg', responseMessage, m, null, fake, false, { mentions: stars.parseMention(responseMessage) })
+
+}
+
+handler.help = ['bots']
+handler.tags = ['serbot']
+handler.command = ['listjadibot', 'bots']
+
+export default handler
