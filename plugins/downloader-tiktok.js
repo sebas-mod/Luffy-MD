0211af871b2fa90375b0d3a12c8eb7168d350c4a
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-17 14:45:21 -0400

    Actualizar downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -30,29 +30,19 @@ var handler = async (m, { conn, args, usedPrefix, command }) => {
         const videoURLWatermark = tiktokData.data.wmplay;
         let txt = '';
         txt += `*\`[ TIKTOK DOWNLOAD ]\`*\n\n`;
-        txt += `> 🤍 *\`» Título :\`* ${title || '❌'}\n`;
-        txt += `> 🤍 *\`» Autor :\`* ${author || '❌'}\n`;
-        txt += `> 🤍 *\`» Visitas :\`* ${views || '❌'}\n`;
-        txt += `> 🤍 *\`» Likes :\`* ${like || '❌'}\n`; 
-        txt += `> 🤍 *\`» Comentarios :\`* ${comment || '❌'}\n`;
-        txt += `> 🤍 *\`» Publicado :\`* ${published || '❌'}\n\n`;
+        txt += `> 🤍 *\`» Título :\`* ${tiktokData.data.title}\n`;
+        txt += `> 🤍 *\`» Autor :\`* ${tiktokData.data.author.nickname || "No info"}\n`;
+        txt += `> 🤍 *\`» Visitas :\`* ${tiktokData.data.play_count}\n`;
+        txt += `> 🤍 *\`» Likes :\`* ${tiktokData.data.digg_count}\n`; 
+        txt += `> 🤍 *\`» Comentarios :\`* ${tiktokData.data.comment_count}\n`;
+        txt += `> 🤍 *\`» Descargas :\`* ${tiktokData.data.download_count}\n\n`;
         txt += '> ©️ ρσωε૨ ɓყ ɠαℓαאყ ƭεαɱ\n';
 
-//VIDEO TIKTOK
-        await conn.sendFile(m.chat, video, 'tiktok.mp4', txt, m, null, rcanal);
-
-
-        const txt = `> 🤍 *\`» Título :\`* ${tiktokData.data.title}\n> 🤍 *\`» Autor :\`* ${tiktokData.data.author.nickname || "No info"
-            }\n> 🤍 *\`» Visitas :\`* ${tiktokData.data.play_count} \n*✧ Publicado:* ${tiktokData.data.create_time
-            }\n\n*✧ Estado:*\n=====================\nLikes = ${tiktokData.data.digg_count
-            }\nComentarios = ${tiktokData.data.comment_count}\nCompartidas = ${tiktokData.data.share_count
-            }\nVistas = \nDescargas = ${tiktokData.data.download_count
-            }\n=====================\n\n(${tiktokData.data.author.unique_id} - https://www.tiktok.com/@${tiktokData.data.author.unique_id
-            } )\n*✧ Sonido:* ${tiktokData.data.music
-            }\n`;
 
         if (videoURL || videoURLWatermark) {
-            await conn.sendFile( m.chat, videoURL, "tiktok.mp4", "`DESCARGA DE TIKTOK`"+`\n\n${txt}`, m, null, rcanal );
+          //  await conn.sendFile( m.chat, videoURL, "tiktok.mp4", "`DESCARGA DE TIKTOK`"+`\n\n${txt}`, m, null, rcanal );
+
+            await conn.sendFile(m.chat, videoURL, 'tiktok.mp4', txt, m, null, rcanal);
 
        await conn.sendMessage(m.chat, { audio: { url: videoURL }, mimetype: "audio/mp4", fileName: tiktokData.data.title + '.mp3' }, { quoted: m })
             setTimeout(async () => {
