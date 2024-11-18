be69b04fef5cb4fe4c347332f8c5f9d1272ba2a2
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-18 16:17:22 -0400

    Actualizar rpg-deposit.js

--- plugins/rpg-deposit.js
+++ plugins/rpg-deposit.js
@@ -2,22 +2,22 @@
 
 let handler = async (m, { args }) => {
    let user = global.db.data.users[m.sender]
-   if (!args[0]) return m.reply('✧ Ingresa la cantidad de *Eris* que deseas Depositar.')
-   if ((args[0]) < 1) return m.reply('✧ Ingresa una cantidad válida de *Eris*.')
+   if (!args[0]) return m.reply('✧ Ingresa la cantidad de *Corazones* que deseas Depositar.')
+   if ((args[0]) < 1) return m.reply('✧ Ingresa una cantidad válida de *Corazones*.')
    if (args[0] == 'all') {
       let count = parseInt(user.limit)
-      user.limit -= count * 1
+      user.corazones -= count * 1
       user.bank += count * 1
-      await m.reply(`Depositaste *${count} Eris* al Banco.`)
+      await m.reply(`Depositaste *${count} Corazones* al Banco.`)
       return !0
    }
    if (!Number(args[0])) return m.reply('✧ La cantidad deve ser un Numero.')
    let count = parseInt(args[0])
-   if (!user.limit) return m.reply('No tienes *Eris* en la Cartera.')
-   if (user.limit < count) return m.reply(`Solo tienes *${user.limit} Eris* en la Cartera.`)
-   user.limit -= count * 1
+   if (!user.corazones) return m.reply('No tienes *corazones* en la Cartera.')
+   if (user.corazones < count) return m.reply(`Solo tienes *${user.limit} corazones* en la Cartera.`)
+   user.corazones -= count * 1
    user.bank += count * 1
-   await m.reply(`Depositaste *${count} Eris* al Banco.`)
+   await m.reply(`Depositaste *${count} corazones* al Banco.`)
 }
 
 handler.help = ['depositar']
