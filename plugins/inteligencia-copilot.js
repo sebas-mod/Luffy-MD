690aed88757653533a06043553d549a693e4e17a
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-18 15:02:57 -0400

    Actualizar inteligencia-copilot.js

--- plugins/inteligencia-copilot.js
+++ plugins/inteligencia-copilot.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
 if (!text) return m.reply('Ingresa un texto')
 
 try {
-let prompt = 'Eres HasumiBot, Tu Creador Es JTXS'
+let prompt = 'Eres Genesis, Tu Creador Es Angel'
 let api = await fetch(`https://api.ryzendesu.vip/api/ai/llama?text=${text}&prompt=${prompt}&models=llama-3.1-70b-instruct`)//Modelo 2 : llama-3.2-11b-vision-instruct
 let json = await api.json()
 let { result } = json
