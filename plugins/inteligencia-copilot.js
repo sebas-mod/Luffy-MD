8c2cb3cf86fcf4b13481b8294f8ac1a5184e651e
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-18 15:05:18 -0400

    Actualizar inteligencia-copilot.js

--- plugins/inteligencia-copilot.js
+++ plugins/inteligencia-copilot.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+/* import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
 if (!text) return m.reply('Ingresa un texto')
@@ -15,4 +15,54 @@ console.error(error)
 
 handler.command = ['llamaAi']
 
+export default handler */
+
+
+import fetch from "node-fetch"
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args.length) return conn.reply(m.chat, `Gunakan format: ${usedPrefix + command} [deskripsi]`, m)
+  let inputText = args.join(" ")
+  try {
+  await m.reply(wait)
+    let ress = await AimusicLyrics(inputText)
+    if (!ress) return await m.reply("Failed.")
+   await m.reply(`*\`AI MUSIC\`*\n\n> Judul: ${inputText}\n- Lyrics:\n\`\`\`${ress}\`\`\``)
+  } catch (e) {
+    throw e
+  }
+}
+
+handler.help = ["aimusiclyrics"]
+handler.tags = ["ai"]
+handler.command = /^(aimusiclyrics)$/i
 export default handler
+
+async function AimusicLyrics(prompt) {
+  const url = "https://aimusic.one/api/v3/lyrics/generator"
+  const headers = {
+    "Content-Type": "application/json",
+    "User-Agent":
+      "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Mobile Safari/537.36",
+    Referer: "https://aimusic.one/ai-lyrics-generator"
+  }
+  const data = {
+    description: prompt,
+    style: "Auto",
+    topic: "Auto",
+    mood: "Auto",
+    lan: "auto",
+    isPublic: true
+  }
+  try {
+    let response = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data)
+    })
+    let result = await response.json()
+    return result.lyrics
+  } catch (e) {
+    throw e
+  }
+}
