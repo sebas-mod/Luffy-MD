e2a63733b7bf4c01a6d4067fe5fcd3a77fbf59bf
sebas-mod <luisrojas30921@gmail.com>
2024-11-21 22:19:13 -0300

    Update start-verificar.js

--- plugins/start-verificar.js
+++ plugins/start-verificar.js
@@ -6,12 +6,12 @@ let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  if (user.registered === true) return m.reply(`🤍 Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg* <Número de serie>`)
-  if (!Reg.test(text)) return m.reply(`🤍 Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.18*`)
+  if (user.registered === true) return m.reply(`🏴‍☠️ Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg* <Número de serie>`)
+  if (!Reg.test(text)) return m.reply(`🏴‍☠️ Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.18*`)
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) return m.reply('🤍 El nombre no puede estar vacío.')
-  if (!age) return m.reply('🤍 La edad no puede estar vacía.')
-  if (name.length >= 100) return m.reply('🤍 El nombre es demasiado largo.' )
+  if (!name) return m.reply('🏴‍☠️ El nombre no puede estar vacío.')
+  if (!age) return m.reply('🏴‍☠️ La edad no puede estar vacía.')
+  if (name.length >= 100) return m.reply('🏴‍☠️ El nombre es demasiado largo.' )
   age = parseInt(age)
   if (age > 100) return m.reply('👴🏻 Wow el abuelo quiere jugar al bot.')
   if (age < 5) return m.reply('🚼  hay un abuelo bebé jsjsj. ')
@@ -20,7 +20,7 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)        
-  let img = await (await fetch(`https://pomf2.lain.la/f/re8oi2mr.jpg`)).buffer()
+  let img = await (await fetch(`https://pomf2.lain.la/f/4078n2i9.jpg`)).buffer()
   let txt = '*`📄 R E G I S T R O 📄`*\n'
       txt += `\`━━━━━━━━━━━━━━━━━━━━\`\n`
       txt += `*\`⁘ NOMBRE:\`* ${name}\n`
