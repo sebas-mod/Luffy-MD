220828f51b1e734a6d59341270a66483684a2094
sebas-mod <luisrojas30921@gmail.com>
2024-11-25 00:36:16 -0300

    Update group-notify.js

--- plugins/group-hidetag.js
+++ plugins/group-hidetag.js
@@ -1,48 +1,27 @@
+import MessageType from '@whiskeysockets/baileys'
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
+
 let handler = async (m, { conn, text, participants }) => {
-	const fkontak = {
-		"key": {
-			"participants": "0@s.whatsapp.net",
-			"remoteJid": "status@broadcast",
-			"fromMe": false,
-			"id": "Halo"
-		},
-		"message": {
-			"contactMessage": {
-				"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-			}
-		},
-		"participant": "0@s.whatsapp.net"
-	}
-
-/*conn.sendMessage(m.chat, {
- text: text, 
- mentions: 
- participants.map(a => a.id) 
- }, {
-  quoted: fkontak 
-  })*/
-let mem = m.isGroup ? await participants.map(a => a.id) : ""
-conn.sendMessage(m.chat, {
-	text: `@${m.chat}`,
-	contextInfo: {
-mentionedJid: mem, 
-		groupMentions: [
-			{
-				groupSubject: `-LuffyBot-Dios[ *${text}* ]`,
-				groupJid: m.chat,
-			},
-		],
-	},
-});
+let users = participants.map(u => conn.decodeJid(u.id))
+let q = m.quoted ? m.quoted : m
+let c = m.quoted ? m.quoted : m.msg
+const msg = conn.cMod(m.chat,
+generateWAMessageFromContent(m.chat, {
+[c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
+text: c || ''
 }
-
-handler.help = ['hidetag']
+}, {
+quoted: m,
+userJid: conn.user.id
+}),
+text || q.text, conn.user.jid, { mentions: users }
+)
+await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+}
+handler.help = ['notify']
 handler.tags = ['group']
-handler.command = /^(hidetag|notify|n)$/i
-
+handler.command = ['n', 'notify'] 
 handler.group = true
 handler.admin = true
 
 export default handler
-
-//xd
